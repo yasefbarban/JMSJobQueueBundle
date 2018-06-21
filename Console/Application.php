fbce2492d6e3c36ae2619352d4a0c39ccb9c585d
Yasef Barban Freixas <yasef.bfreixas@gmail.com>
2018-06-21 16:04:42 -0500

    removing stackTrace

--- Console/Application.php
+++ Console/Application.php
@@ -84,18 +84,16 @@ class Application extends BaseApplication
         }
 
         $this->getConnection()->executeUpdate(
-            "UPDATE jms_jobs SET stackTrace = :trace, memoryUsage = :memoryUsage, memoryUsageReal = :memoryUsageReal WHERE id = :id",
+            "UPDATE jms_jobs SET  memoryUsage = :memoryUsage, memoryUsageReal = :memoryUsageReal WHERE id = :id",
             array(
                 'id' => $jobId,
                 'memoryUsage' => memory_get_peak_usage(),
                 'memoryUsageReal' => memory_get_peak_usage(true),
-                'trace' => serialize($ex ? FlattenException::create($ex) : null),
             ),
             array(
                 'id' => \PDO::PARAM_INT,
                 'memoryUsage' => \PDO::PARAM_INT,
                 'memoryUsageReal' => \PDO::PARAM_INT,
-                'trace' => \PDO::PARAM_LOB,
             )
         );
     }
